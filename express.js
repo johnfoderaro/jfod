e3a657b4aa7d61ae50abb25471b21b98d31981f5
John Foderaro <johnfoderaro@icloud.com>
2016-10-25 17:23:19 -0400

    contact form updates, style updates

--- express.js
+++ express.js
@@ -1,74 +1,20 @@
 'use strict';
 const gutil = require('gulp-util');
-const nodemailer = require('nodemailer');
 const express = require('express');
 const expressValidator = require('express-validator');
 const helmet = require('helmet');
-const app = express();
-const port = 3000;
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const sendMail = require('./lib/express/send-mail');
+const validators = require('./lib/express/validators');
+const app = express();
 const form = multer();
-const fs = require('fs');
-
-//TODO send reponse data to browser
-//TODO validation and sanitizing of user input
+const port = 3000;
 
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(expressValidator());
+app.use(expressValidator(validators));
 app.post('/submit', form.array(), sendMail);
 app.use(express.static(__dirname + '/build/'));
 app.listen(port, () => gutil.log(`Express listening on port ${port}`));
-
-function sendMail(request, response) {
-  // Check honeypot
-  request.checkBody('name', 'Invalid entry.').isAlpha();
-  const error = request.validationErrors();
-  if (error) {
-    console.error(error);
-    response.send(error);
-    return;
-  }
-  const transporter = nodemailer.createTransport({
-    aliases: ["Me", "Mac"],
-    domains: ["me.com", "mac.com"],
-    host: "smtp.mail.me.com",
-    port: 587,
-    auth: {
-      user: readConfig('icloud'),
-      pass: readConfig('password')
-    }
-  });
-  const mailOptions = {
-    from: readConfig('email'),
-    to: readConfig('email'),
-    subject: 'New Jfods.me Submission',
-    html: `<h2>From</h2>
-           <p>${request.body.name}</p>
-           <h2>Reply To</h2>
-           <p>${request.body.email}</p>
-           <h2>Comment</h2>
-           <p>${request.body.comment}</p>`
-};
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      response.send(error);
-    } else {
-      response.send(info);
-    }
-  });
-}
-
-function readConfig(item) {
-  const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-  switch(item) {
-    case 'icloud':
-  return config.icloud;
-    case 'password':
-  return config.password;
-    case 'email':
-  return config.email;
-  }
-}
--- src/js/src/form.js
+++ src/js/src/form.js
@@ -1,46 +1,77 @@
 'use strict';
-//TODO log/handle server response
-//TODO highlight errors
-//TODO sucess message, etc.
+
 (() => {
+  // Check if #contact-form is present
   if (document.getElementById('contact-form') !== null) {
+    // Set an on submit event
     document.getElementById('contact-form').onsubmit = (event) => {
       event.preventDefault();
       const form = event.target;
       const data = new FormData(form);
       const xhr  = new XMLHttpRequest();
-      // Validate form
-      if (validator(form)) {
-        xhr.open('post', '/submit', true);
-        xhr.send(data);
-        console.log(xhr.response);
-        console.log(xhr.responseText);
-      }
+      xhr.open('post', '/submit', true);
+      xhr.send(data);
+      // Update the send button to indicate progress
+      document.getElementById('submit').textContent = 'Sending...';
+      document.getElementById('submit').disabled = true;
+      // Handle the response
+      xhr.onreadystatechange = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE) {
+          highlighter(JSON.parse(xhr.responseText));
+        }
+      };
     };
   }
-  function validator(form) {
-    let flag = true;
-    const check = ['name', 'email', 'comment'];
-    for (let i = 0; i < form.length; i++) {
-      // Sanitize input
-
-      // Check for something that resembles an email address
-      if (form[i].name === 'email' && !/(.+)@(.+){2,}\.(.+){2,}/.test(form[i].value)) {
-        flag = false;
+  function highlighter(response) {
+    const form = document.getElementById('contact-form');
+    const fields = ['name', 'email', 'comment'];
+    // Change send button back to normal
+    document.getElementById('submit').textContent = 'Send';
+    document.getElementById('submit').disabled = false;
+    if (response.validator) {
+      // Remove any existing error classes
+      for (let i = 0; i < fields.length; i++) {
+        let input = document.getElementById(fields[i]);
+        let label = input.previousElementSibling;
+        input.className = '';
+        label.className = '';
       }
-      // Check honey pot
-      if (form[i].name === 'topic' && form[i].value.length > 0) {
-        flag = false;
-      }
-      // Check visible fields for input
-      for (let n = 0; n < check.length; n++) {
-        if (form[i].value === '' && form[i].name === check[n]) {
-          flag = false;
+      // Add classes to error items
+      for (let i = 0; i < response.validator.length; i++) {
+        let form = document.getElementById('contact-form');
+        let input = document.getElementById(response.validator[i].param);
+        let label = input.previousElementSibling;
+        input.className = 'error-input';
+        label.className = 'error-text';
+        // Display form message
+        if (response.validator.length === i + 1) {
+          if (document.getElementById('form-message')) {
+            document.getElementById('form-message').parentNode
+            .removeChild(document.getElementById('form-message'));
+          }
+          form.insertAdjacentHTML('beforeend', `<p id="form-message" class="error-text">${response.validator[i].msg}</p>`);
         }
       }
-      // Return once we finish looping
-      if (form.length === i + 1) {
-        return flag;
+    }
+    if (response.status) {
+      // Remove any existing error classes
+      for (let i = 0; i < fields.length; i++) {
+        let input = document.getElementById(fields[i]);
+        let label = input.previousElementSibling;
+        input.className = '';
+        label.className = '';
+      }
+      // Remove any existing messages
+      if (document.getElementById('form-message')) {
+        document.getElementById('form-message').parentNode
+        .removeChild(document.getElementById('form-message'));
+      }
+      // Format and display message
+      if (response.status.success) {
+        form.insertAdjacentHTML('beforeend', `<p id="form-message" class="success-text">${response.status.success}</p>`);
+      }
+      if (response.status.error) {
+        form.insertAdjacentHTML('beforeend', `<p id="form-message" class="error-text">${response.status.error}</p>`);
       }
     }
   }
